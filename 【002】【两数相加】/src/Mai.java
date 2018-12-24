b2b518f2c679708168b8b72e9dd14a17a4ecb590
Breath-Zero <1395043294@qq.com>
2018-12-24 21:37:05 +0800

    <LeedCode-更新-两数相加> 2018/12/24 <叶>

---
+++ 【002】【两数相加】/src/Mai.java
@@ -0,0 +1,94 @@
+/**
+ * 原题要求：
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 解题思路：
+ * 对两个链表都从第一个开始处理，进行相加，结果再除以10求商，作为下一位相加的进位，
+ * 同时记录余数，作为本位的结果，一直处理，直到所有的结点都处理完。
+ *
+ * @ClassName: Mai
+ * @Author: Mr.Ye
+ * @Data: 2018-12-24 21:33
+ **/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode root = new ListNode(0); // 头结点
+        ListNode r = root;
+        root.next = l1;
+
+        int carry = 0; // 初始进位
+        int sum;
+        while (p1 != null && p2 != null) {
+            sum = p1.val + p2.val + carry;
+            p1.val = sum % 10; // 本位的结果
+            carry = sum / 10; // 本次进位
+
+            r.next = p1;
+            r = p1; // 指向最后一个相加的结点
+            p1 = p1.next;
+            p2 = p2.next;
+
+        }
+
+        if (p1 == null) {
+            r.next = p2;
+        } else {
+            r.next = p1;
+        }
+
+        // 最后一次相加还有进位
+        if (carry == 1) {
+            // 开始时r.next是第一个要相加的结点
+            while (r.next != null) {
+                sum = r.next.val + carry;
+                r.next.val = sum % 10;
+                carry = sum / 10;
+                r = r.next;
+            }
+
+            // 都加完了还有进位，就要创建一个新的结点
+            if (carry == 1) {
+                r.next = new ListNode(1);
+            }
+        }
+
+        return root.next;
+    }
+}
+
+public class Mai {
+    public static void main(String[] args) {
+
+    }
+}
